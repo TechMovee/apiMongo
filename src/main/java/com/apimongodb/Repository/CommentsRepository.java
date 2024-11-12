5d38aada397f33b5c3aba0bf49dc78e74c84d0c7
Beatriz Belaparte Favero <beatrizbelaparte@gmail.com>
2024-11-12 10:34:55 -0300

    aaaaa

--- src/main/java/com/apimongodb/Repository/CommentsRepository.java
+++ src/main/java/com/apimongodb/Repository/CommentsRepository.java
@@ -1,7 +1,21 @@
+//package com.apimongodb.Repository;
+//
+//import com.apimongodb.Model.Comments;
+//import org.springframework.data.mongodb.repository.MongoRepository;
+//
+//public interface CommentsRepository extends MongoRepository<Comments, String> {
+//
+//}
 package com.apimongodb.Repository;
 
 import com.apimongodb.Model.Comments;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface CommentsRepository extends MongoRepository<Comments, String> {
+    List<Comments> findByCpf(String cpf);
 }
+
